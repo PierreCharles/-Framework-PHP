984774d6dcf4e6089a8232f40f5439cb6a21afce
Pierre <pier.charles@hotmail.fr>
2016-02-12 16:16:04 +0100

    Add new finder test

---
+++ tests/ModelTest/FinderTest/StatusFinderTest.php
@@ -0,0 +1,50 @@
+<?php
+
+use Model\DataBase\DatabaseConnection;
+use Model\Finder\StatusFinder;
+use Model\Entity\Status;
+
+
+class StatusFinderTest extends TestCase
+{
+    private $connection;
+    private $finder;
+    public function setUp(){
+        $this->connection = new DatabaseConnection('sqlite::memory:');
+        $this->connection->exec(<<<SQL
+CREATE TABLE IF NOT EXISTS STATUSES (
+  status_id INT PRIMARY KEY NOT NULL,
+  status_message VARCHAR(140) NOT NULL,
+  status_user_name VARCHAR(100),
+  status_date DATETIME NOT NULL
+);
+
+INSERT INTO `statuses` (`status_id`, `status_message`, `status_user_name`, `status_date`) VALUES
+(1, 'Mon premier tweet !', 'UserTest', '2016-02-11 00:19:39');
+SQL
+        );
+        $this->finder = new StatusFinder($this->connection);
+    }
+
+    public function testCountFindAll(){
+        $statuses = $this->finder->findAll();
+        $this->assertEquals(1,count($statuses));
+    }
+
+    public function testFindAll(){
+        $expected = new Status(1, 'Mon premier tweet !', 'UserTest', date('2016-02-11 00:19:39'));
+        $statuses = $this->finder->findAll();
+        $this->assertEquals($expected,$statuses);
+    }
+
+    public function testCountFindOneById(){
+        $status = $this->finder->findOneById(1);
+        $this->assertEquals(1,count($status));
+    }
+
+    public function testFindOneById(){
+        $expected = new Status(1, 'Mon premier tweet !', 'UserTest', date('2016-02-11 00:19:39'));
+        $status = $this->finder->findOneById(1);
+        $this->assertEquals($expected, $status);
+    }
+}
---
+++ tests/ModelTest/FinderTest/UserFinderTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use Model\Finder\UserFinder;
+use Model\DataBase\DatabaseConnection;
+use Model\Entity\User;
+
+class UserFinderTest extends TestCase
+{
+    private $connection;
+    private $finder;
+    public function setUp(){
+        $this->con = new DatabaseConnection('sqlite::memory:');
+        $this->con->exec(<<<SQL
+CREATE TABLE IF NOT EXISTS USER (
+      user_id INT PRIMARY KEY NOT NULL,
+      user_name VARCHAR(100) NOT NULL,
+      user_password VARCHAR(100) NOT NULL
+);
+
+INSERT INTO `user` (`user_id`, `user_name`, `user_password`) VALUES
+(1, 'UserTest', 'PasswordTest');
+SQL
+        );
+        $this->finder = new UserFinder($this->connection);
+    }
+
+    public function testFindOneByIdCount(){
+        $user = $this->finder->findOneById(1);
+        $this->assertEquals(1,count($user));
+    }
+
+    public function testFindOneById(){
+        $expected = new User('1','UserTest','PasswordTest');
+        $user = $this->finder->findOneById(1);
+        $this->assertEquals($expected, $user);
+    }
+
+    public function testFindOneByUserNameCount(){
+        $user = $this->finder->findOneByName('toto');
+        $this->assertEquals(1,count($user));
+    }
+
+    public function testFindOneByName(){
+        $expected = new User('1','UserTest','PasswordTest');
+        $user = $this->finder->findOneByUserName('UserTest');
+        $this->assertEquals($expected,$user);
+    }
+}
